e957b44122f0da8eea0218a13ddbbb65d08f40ad
409411716 <409411716@gms.tku.edu.tw>
2022-03-22 16:02:25 +0800

    initial crown2_16 files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const crown_16_Router = require('./routes/crown_16');
+const crown2_16_Router = require('./routes/crown2_16');
 var app = express();
 
 // view engine setup
@@ -23,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/crown_16', crown_16_Router);
-
+app.use('/crown2_16', crown2_16_Router);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
